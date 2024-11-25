ca998eaf782829a985241188ce37240cecdcfbdc
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-11-26 03:39:11 +0800

    lambda-based filtering

--- cmd/roundtrip.go
+++ cmd/roundtrip.go
@@ -12,11 +12,28 @@ func main() {
 	destination := flag.String("destination", "", "Path to the destination file")
 	var discards []string
 	flag.Var((*stringsFlag)(&discards), "discard", "List of object names to delete")
+	var keeps []string
+	flag.Var((*stringsFlag)(&keeps), "keep", "List of object names to keep -- only applicable if non-empty")
 
 	// Parse flags
 	flag.Parse()
 
-	obj, err := magica.FromFileWithLayersAndDisallowedLayerNames(*source, []int{}, discards)
+	obj, err := magica.FromFileWithLayersAndFilter(*source, []int{}, func(layerName string) bool {
+		if len(keeps) > 0 {
+			for _, keptName := range keeps {
+				if keptName == layerName {
+					return true
+				}
+			}
+			return false
+		}
+		for _, discardedName := range discards {
+			if discardedName == layerName {
+				return false
+			}
+		}
+		return true
+	})
 	if err != nil {
 		log.Fatalf("could not read file: %s", err)
 	}
